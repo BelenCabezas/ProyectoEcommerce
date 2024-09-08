de1506d4dd41647343c02e937030610516b984c0
Belén Cabezas <0cabezasbelen0@gmail.com>
2024-09-08 17:49:03 -0300

    terminada sub pagina pan

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.listen(4500,()=>{
 //Sub pagina de receta -> red velvet
 app.get('/RecetaVelvet',(req,res)=>{
     res.render('RecetaVelvet')
+}
+)
+//Sub pagina de receta -> pan
+app.get('/RecetaPan',(req,res)=>{
+    res.render('RecetaPan')
 })
 
 //Ruta Productos
