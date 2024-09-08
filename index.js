322a3cec52c20605e166130babede94e2a3f999c
Belén Cabezas <0cabezasbelen0@gmail.com>
2024-09-08 18:37:15 -0300

    terminadas sub paginas

--- index.js
+++ index.js
@@ -20,17 +20,39 @@ app.listen(4500,()=>{
  app.get('/Recetas',(req,res)=>{
     res.render('Recetas')
 })
+//Ruta Productos
+app.get('/Productos',(req,res)=>{
+    res.render('Productos')
+})
+
+/**********SUB PAGINAS**********/
+
 //Sub pagina de receta -> red velvet
 app.get('/RecetaVelvet',(req,res)=>{
     res.render('RecetaVelvet')
-}
-)
+})
+
 //Sub pagina de receta -> pan
 app.get('/RecetaPan',(req,res)=>{
     res.render('RecetaPan')
 })
 
-//Ruta Productos
-app.get('/Productos',(req,res)=>{
-    res.render('Productos')
+//Sub pagina de receta -> croisant
+app.get('/RecetaCroisant',(req,res)=>{
+    res.render('RecetaCroisant')
+})
+
+//Sub pagina de receta -> brownie
+app.get('/RecetaBrownie',(req,res)=>{
+    res.render('RecetaBrownie')
+})
+
+//Sub pagina de receta -> tiramisu
+app.get('/RecetaTiramisu',(req,res)=>{
+    res.render('RecetaTiramisu')
+})
+
+//Sub pagina de receta -> tiramisu
+app.get('/RecetaMacarons',(req,res)=>{
+    res.render('RecetaMacarons')
 })
