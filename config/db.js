753d77dfb6111b041a349c25ce039853295d6d76
ChrisChamizo <christianchamizo@live.com.ar>
2024-10-07 11:25:51 -0300

    Se conecto la base de datos a mongo atlas. Se modificó la URL de mongo

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-//requiero el uso de mongoose
 const mongoose = require('mongoose');
+require('dotenv').config(); // Cargar variables de entorno
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("Base de datos conectada correctamente".green);
+        console.log("Base de datos conectada correctamente");
     } catch (error) {
         console.error("Error al conectar la base de datos:", error);
         process.exit(1); // Detenemos la aplicación si falla la conexión
     }
 };
 
+// Exportamos la función para usarla en otros archivos
 module.exports = connectDB;
